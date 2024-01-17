1669d6c759ae7d49e6858ce59ae939eeb788ba03
jjjfay <juliacfay3@gmail.com>
2024-01-16 18:12:11 -0800

    Update step_response.py

--- Lab 0/src/step_response.py
+++ Lab 0/src/step_response.py
@@ -8,7 +8,7 @@ micropython.alloc_emergency_exception_buf(100)
 #define pin names 
 
 pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.OUT_PP)
-pinB0ADC = pyb.ADC(pyb.Pin.board.PC0)
+pinB0ADC = pyb.ADC(pyb.Pin.board.PB0)
 
 #initialize queues 
 
@@ -19,7 +19,9 @@ value_queue = cqueue.IntQueue(QUEUE_SIZE) #from 405 library documentation
 for i in range(QUEUE_SIZE):
     time.append(0.01*i)
     
-tim = pyb.Timer(2, freq=1000)      # create a timer object using timer 4 - trig
+    #while printing create the time variables 
+    
+tim = pyb.Timer(2, freq=100)      # create a timer object using timer 4 - trig
 
 #create interrupt
 
@@ -37,18 +39,19 @@ def timer_int(tim_num):
 
 def step_response():                 
     pinC0.value(0)
-    #tim = pyb.Timer(4, freq=1000)   # create a timer object using timer 4 - trig
     tim.callback(timer_int)          # set the callback to our timer_int function
     pinC0.value(1)
     
     while not value_queue.full():
         pass              
     
-    print(value_queue)
+    #run a for loop through time run get on the queue
+    for val in time:
+        print(f"{val},{value_queue.get(val)) #not sure if this is correct     
+    #print(value_queue)
     print("end")
     
 
-#if __name__ == "__main__":
-
-step_response()
+if __name__ == "__main__": #only runs if its the main program 
+    step_response()
 
