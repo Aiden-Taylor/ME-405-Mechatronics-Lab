2c3dac0f34069d4a638c14a4dee213f32d5ce335
jjjfay <juliacfay3@gmail.com>
2024-02-27 00:14:37 -0800

    Updates to doxygen comments

--- src/controller.py
+++ src/controller.py
@@ -25,6 +25,42 @@ class P_Control():
         This includes inputs for the motor pins and encoder pins used in the encoder_reader and motor_driver 
         imported classes. Additionally, the setpoint and Kp values are initialized, as well as the time and 
         position arrays that will be used within the P_control class. 
+        
+        @param in_Kp The proportional gain constant used to calculate the PWM signal sent to the motor. 
+        
+        @param in_setpoint The setpoint value is the desired position of the motor. The 
+               current position of the motor is compared to the setpoint to calculate the 
+               error. 
+        
+        @param enp1 The value sent to the enable pin controlled by the microcontroller. When the en_pin
+               is set high, it enables the motor. 
+               
+        @param in1pwm The initial pwm percentage value sent to the timer 
+               channel corresponding to the first motor pin. When in1pwm 
+               is set to 0, the motor runs in the forward direction.  
+        
+        @param in2pwm The initial pwm percentage value sent to the timer 
+               channel corresponding to the second motor pin. When in2pwm 
+               is set to 0, the motor runs in the reverse direction. 
+        
+        @param timer The timer channel that corresponds with the motor pins 
+               used for the motor. 
+        
+        @param enapin The chosen enable pin number controlled by the microcontroller corresponding 
+               to EN_A/OCD_A or EN_B/OCD_B on the L6206. 
+        
+        @param mp1 The first chosen motor pin on the CPU corresponding to the IN1 A/B pin 
+               on the L6206.
+        
+        @param mp2 The second chosen motor pin on the CPU corresponding to the IN2_A/B pin 
+               on the L6206.
+               
+        @param coderp1 The encoder pin on the L6206 corresponding to channel A for the encoder. 
+        
+        @param coderp2 The encoder pin on the L6206 corresponding to channel B for the encoder. 
+        
+        @param codertimer The timer chanel corresponding to the selected encoder pins.  
+        
         """
         
         #initialize values for setpoint, gain
@@ -55,6 +91,8 @@ class P_Control():
             value is saturated to either -100 or 100. Then the set_duty_cycle function 
             imported from the motor_driver class is run. The calculated PWM signal is 
             returned at the end of the funciton. 
+            
+            @return The PWM signal calcuated by the P control loop is returned. 
             """  
         self.setpoint = in_setpoint 
         encout = self.enco1.read()
